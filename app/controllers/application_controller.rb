32c3a72c5dab4349dd5f52ad99ec6b4809128342
Ted Martin <etmartinkazoo@gmail.com>
2024-02-05 12:53:24 -0500

    Update versions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,6 @@ class ApplicationController < ActionController::Base
   private
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_back(fallback_location: todos_path)
+    redirect_back(fallback_location: root_path)
   end
 end
